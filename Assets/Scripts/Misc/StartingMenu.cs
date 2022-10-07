6d895856aaedbf7bc5ffa678e8aa4ad0b2814321
Stephen Hubbard <hubbardgames@gmail.com>
2022-10-07 16:09:09 -0600

    options menu

--- Assets/Scripts/Misc/StartingMenu.cs
+++ Assets/Scripts/Misc/StartingMenu.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartingMenu : MonoBehaviour
 {
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
     [SerializeField] private GameObject worldSpaceCavas;
     [SerializeField] private GameObject uiCanvas;
     [SerializeField] private GameObject startingMenuCanvas;
+    [SerializeField] private GameObject optionsContainer;
+    [SerializeField] private GameObject mainMenuContainer;
+    [SerializeField] private TMP_Text resText;
+    [SerializeField] private GameObject logo;
+    [SerializeField] private Toggle fullScreenTog;
+    [SerializeField] private List<ResItem> resolutions = new List<ResItem>();
+    public int selectedResolution = 0;
     
     private Animator myAnimator;
 
@@ -17,6 +28,41 @@ public class StartingMenu : MonoBehaviour
 
     private void Start() {
         InputManager.instance.isOnMainMenu = true;
+
+        if (fullScreenTog.isOn) {
+            Screen.fullScreen = true;
+        }
+
+        DetectCurrentRes();
+    }
+
+    public void UpdateMusicVolume() {
+        AudioListener.volume = musicSlider.value;
+    }
+
+    public void UpdateSFXVolume() {
+        AudioListener.volume = sfxSlider.value;
+    }
+
+    private void DetectCurrentRes() {
+        bool foundRes = false;
+        for (int i = 0; i < resolutions.Count; i++)
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical) {
+                foundRes = true;
+                selectedResolution = i;
+                UpdateResText();
+            }
+        }
+
+        if (!foundRes) {
+            ResItem newRes = new ResItem();
+            newRes.horizontal = Screen.width;
+            newRes.vertical = Screen.height;
+            resolutions.Add(newRes);
+            selectedResolution = resolutions.Count - 1;
+            UpdateResText();
+        }
     }
 
     public void NewGameButton() {
@@ -34,6 +80,47 @@ public class StartingMenu : MonoBehaviour
 
     public void OptionsButton() {
         AudioManager.instance.Play("UI Click");
+        optionsContainer.SetActive(true);
+        mainMenuContainer.SetActive(false);
+        logo.SetActive(false);
+    }
+
+    public void SaveOptionsButton() {
+        AudioManager.instance.Play("UI Click");
+        if (fullScreenTog.isOn) {
+            Screen.fullScreen = false;
+        } else {
+            Screen.fullScreen = true;
+        }
+            
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullScreenTog.isOn);
+    }
+
+    public void BackOptionsButton() {
+        AudioManager.instance.Play("UI Click");
+        optionsContainer.SetActive(false);
+        mainMenuContainer.SetActive(true);
+        logo.SetActive(true);
+    }
+
+    public void UpdateResText() {
+        resText.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+    public void ResLeft() {
+        selectedResolution--;
+        if (selectedResolution < 0) {
+            selectedResolution = 0;
+        }
+        UpdateResText();
+    }
+
+    public void ResRight() {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1) {
+            selectedResolution = resolutions.Count - 1;
+        }
+        UpdateResText();
     }
 
     public void ExitGameButon() {
@@ -77,7 +164,11 @@ public class StartingMenu : MonoBehaviour
 
         DataPersistenceManager.instance.LoadGame();
         InputManager.instance.isOnMainMenu = false;
+    }
 
+    [System.Serializable]
+    public class ResItem {
+        public int horizontal, vertical;
     }
 
 }
